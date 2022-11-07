d62fdd4e6b8f6fa620263c050a8ab69df0e0a2d7
deepak-singh5219 <84671689+deepak-singh5219@users.noreply.github.com>
2022-11-07 10:44:44 +0530

    working on register route

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send('on home route');
 })
+
+app.post('/register', async (req,res) => {
+    const {firstName, lastName, email, password} = req.body();
+
+    if(!(firstName && lastName && email && password)){
+        console.log('All fields are required!');
+        res.status(400).send('Missing Information');
+    }
+
+    const existUser = User.findOne({email});
+    if(existUser){
+        res.status(400).send('User already exists');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const myEncrypPassword = await bcrypt.hash(password,salt);
+
+    const user = {
+        firstName,
+        lastName,
+        email, 
+        password:myEncrypPassword
+    };
+  
+})
