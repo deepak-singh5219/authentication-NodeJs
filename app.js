c6503603dc4ee3d43d116f6ad73b40b4fae23607
deepak-singh5219 <84671689+deepak-singh5219@users.noreply.github.com>
2022-11-09 21:41:13 +0530

    register route done

--- app.js
+++ app.js
@@ -60,4 +60,57 @@ app.post('/register', async (req,res) => {
         console.log(error);
     }
  
+});
+
+app.post('/login', async(req,res) => {
+
+    try {
+        const {email, password} = req.body();
+
+        if(!(email && password)){
+            res.status('401').send('missing information');
+            console.log('information missing');
+        }
+
+        const user = await User.findOne({email});
+        if(!user){
+            res.status(401).send("User not found");
+        }
+
+        // matching the password
+
+        if(user && (await bcrypt.compare(password,user.password))){
+            // credentials are correct and thus logging in user
+            const token = jwt.sign({
+                id:user._id,
+                email
+            }, 'shhhhh', 
+            {
+                expiresIn: "2h"
+            }
+            );
+
+            user.password  = undefined;
+            user.token = token;
+
+            const options = {
+                expires: new Date(Date.now() + 3*24*60*60*1000 ),
+                httpOnly:true,
+            }
+
+            res.status(200).cookie("token", token, options).json({
+                success: true,
+            });
+            
+        }
+
+        res.status(400).send("email or password incorrect");
+
+    } catch (error) {
+      console.log(error);
+    }
+
 })
+
+
+
